6b34b55f116415d0e1a82a42f12a7d064701e8c1
DenisMakovskyi <makovsky.denis@gmail.com>
2019-07-30 15:05:50 +0300

    Turned on GSON lenient mode

--- dlp/src/main/java/ua/com/wl/dlp/core/di/AppModule.kt
+++ dlp/src/main/java/ua/com/wl/dlp/core/di/AppModule.kt
@@ -10,6 +10,8 @@ import org.koin.dsl.module
 
 val appModule = module {
     single {
-        GsonBuilder().create()
+        GsonBuilder()
+            .setLenient()
+            .create()
     }
 }
