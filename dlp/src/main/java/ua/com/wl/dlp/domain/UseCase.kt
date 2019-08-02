468ab91ecc2517f96fbce4d2751045d70065c66b
DenisMakovskyi <makovsky.denis@gmail.com>
2019-08-02 18:13:50 +0300

    Handle api calls exceptions

--- dlp/src/main/java/ua/com/wl/dlp/domain/UseCase.kt
+++ dlp/src/main/java/ua/com/wl/dlp/domain/UseCase.kt
@@ -1,5 +1,6 @@
 package ua.com.wl.dlp.domain
 
+import android.util.Log
 import retrofit2.Response
 
 import ua.com.wl.dlp.data.api.errors.ErrorsMapper
@@ -14,17 +15,23 @@ open class UseCase(private val errorsMapper: ErrorsMapper) {
 
     protected suspend fun <T : Any> callApi(
         call: suspend () -> Response<T>,
-        errorClass: Class<out ApiException>? = null): Result<T> = call.invoke().let {
-        if (it.isSuccessful) {
-            Result.Success(it.body())
-
-        } else {
-            Result.Failure(if (it.errorBody() != null && errorClass != null) {
-                errorsMapper.createApiExceptionFromErrorBody(it.errorBody(), errorClass)
+        errorClass: Class<out ApiException>? = null): Result<T> =
+        try {
+            val response = call.invoke()
+            if (response.isSuccessful) {
+                Result.Success(response.body())
 
             } else {
-                ApiRuntimeException()
-            })
+                Result.Failure(if (response.errorBody() != null && errorClass != null) {
+                    errorsMapper.createApiExceptionFromErrorBody(response.errorBody(), errorClass)
+
+                } else {
+                    ApiRuntimeException()
+                })
+            }
+
+        } catch (e: Exception) {
+            Log.e("DLPCore", "Unexpected error when call api", e)
+            Result.Failure(ApiRuntimeException())
         }
-    }
 }
