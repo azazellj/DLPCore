1916e3dd2dd2f1e971479b987537dd10ce88c4ca
DenisMakovskyi <makovsky.denis@gmail.com>
2019-08-13 15:02:49 +0300

    Updated rest method

--- dlp/src/main/java/ua/com/wl/dlp/data/api/ConsumerApiV1.kt
+++ dlp/src/main/java/ua/com/wl/dlp/data/api/ConsumerApiV1.kt
@@ -3,6 +3,7 @@ package ua.com.wl.dlp.data.api
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 import ua.com.wl.dlp.data.api.requests.consumer.feedback.FeedbackRequest
 import ua.com.wl.dlp.data.api.responses.consumer.feedback.FeedbackResponse
@@ -18,6 +19,6 @@ interface ConsumerApiV1 {
     @GET("api/v1/consumer/qr-code/")
     suspend fun getQrCode(): Response<QrCodeResponse>
 
-    @GET("api/v1/consumer/feedback/")
+    @POST("api/v1/consumer/feedback/")
     suspend fun feedback(@Body request: FeedbackRequest): Response<FeedbackResponse>
 }
