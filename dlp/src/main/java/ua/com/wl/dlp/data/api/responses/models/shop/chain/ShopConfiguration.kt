2c668ff3abae4f84d9858634a425cedb98e1f8d2
srsMADCAT <olegr1998@ukr.net>
2021-02-18 17:24:46 +0200

    ShopConfiguration added delivery_payment options

--- dlp/src/main/java/ua/com/wl/dlp/data/api/responses/models/shop/chain/ShopConfiguration.kt
+++ dlp/src/main/java/ua/com/wl/dlp/data/api/responses/models/shop/chain/ShopConfiguration.kt
@@ -1,29 +1,36 @@
 package ua.com.wl.dlp.data.api.responses.models.shop.chain
 
 import com.google.gson.annotations.SerializedName
+import ua.com.wl.dlp.data.api.responses.models.shop.delivery.PaymentOptions
 
 /**
  * @author Oleh "MADCAT" Rysniuk
  */
 
-data class ShopConfiguration (
-    @SerializedName("allow_pre_orders")
-    var isPreOrdersAllowed: Boolean = false,
+data class ShopConfiguration(
+        @SerializedName("allow_pre_orders")
+        var isPreOrdersAllowed: Boolean = false,
 
-    @SerializedName("allow_pre_order_delivery")
-    var isPreOrderDeliveryAllowed: Boolean = false,
+        @SerializedName("allow_pre_order_delivery")
+        var isPreOrderDeliveryAllowed: Boolean = false,
 
-    @SerializedName("allow_pre_order_takeaway")
-    var isPreOrderTakeawayAllowed: Boolean = false,
+        @SerializedName("allow_pre_order_takeaway")
+        var isPreOrderTakeawayAllowed: Boolean = false,
 
-    @SerializedName("allow_pre_order_in_place")
-    var isPreOrderInPlaceAllowed: Boolean = false,
+        @SerializedName("allow_pre_order_in_place")
+        var isPreOrderInPlaceAllowed: Boolean = false,
 
-    @SerializedName("allow_table_reservation")
-    var isTableReservationAllowed: Boolean = false,
+        @SerializedName("allow_table_reservation")
+        var isTableReservationAllowed: Boolean = false,
 
-    @SerializedName("cash_back_percentage")
-    var cashBackPercentage: Int,
+        @SerializedName("cash_back_percentage")
+        var cashBackPercentage: Int,
 
-    @SerializedName("payment_online_available")
-    var isPaymentOnlineAvailable: Boolean = false)
+        @SerializedName("delivery_payment")
+        val deliveryPaymentOptions: PaymentOptions,
+
+        @SerializedName("delivery_minimal_sum")
+        val deliveryMinimalSum: Double,
+
+        @SerializedName("payment_online_available")
+        var isPaymentOnlineAvailable: Boolean = false)
