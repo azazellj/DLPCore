3c7a7fe7688b9dd41ff1c79af369c579017f82b6
azazellj <azazellj@gmail.com>
2021-10-12 09:19:03 +0300

    added parser values

--- dlp/src/main/java/ua/com/wl/dlp/data/api/responses/models/shop/offer/promo/Permissions.kt
+++ dlp/src/main/java/ua/com/wl/dlp/data/api/responses/models/shop/offer/promo/Permissions.kt
@@ -2,8 +2,10 @@ package ua.com.wl.dlp.data.api.responses.models.shop.offer.promo
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
+@JsonClass(generateAdapter = true)
 @Parcelize
 data class Permissions(
     @Json(name = "pre_order")
